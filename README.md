baea7d79356fef7352ef6f6e633fab1f548f8b05
loljoho <loljoho@users.noreply.github.com>
2019-11-14 19:53:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# my-first-calculator.js
+
+This is [so silly](https://github.com/AceLewis/my_first_calculator.py).
+
+## Usage
+
+Clone the repository and enter the directory:
+```bash
+git clone https://github.com/loljoho/my-first-calculator.git
+cd my-first-calculator/
+```
+
+Install stuff:
+```bash
+npm install
+```
+
+Run it:
+```bash
+npm start
+```
+
+## About
+
+This is such a silly script and is written pretty badly.  
+
+I'm not particularly proud of it.
---
+++ generator.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const filename = 'my-first-calculator.js';
+
+const minNum  = 0;
+const maxNum  = 100;
+const nums    = [...Array(maxNum + 1).keys()];
+const signs   = ['+', '-', '/', '*'];
+
+let outputStr = `// my-first-calculator.js by loljoho
+const readlineSync = require('readline-sync');
+
+console.log('Welcome to this calculator!');
+console.log('It can add, subtract, multiply, and divide whole numbers from ${minNum} to ${maxNum}.');
+let num1 = readlineSync.question('Please choose your first number: ');
+let sign = readlineSync.question('What do you want to do?  +, -, /, or *: ');
+let num2 = readlineSync.question('Please choose your second number: ');\n\n`;
+
+// TODO: do it async without any dependencies
+fs.writeFileSync(filename, outputStr);
+
+// write all the if statements to file
+signs.forEach(sign => {
+  nums.forEach(num1 => {
+    nums.forEach(num2 => {
+      let equals = eval(`${num1}${sign}${num2}`);
+      // No `else if`s used because JavaScript will throw the following error:
+      // RangeError: Maximum call stack size exceeded
+      fs.appendFileSync(filename, 
+        `if (num1 == ${num1} && sign == '${sign}' && num2 == ${num2}) {\n\tconsole.log(\`${num1} ${sign} ${num2} = ${equals}\`);\n}\n`
+      );
+    });
+  });
+});
+
+fs.appendFileSync(filename, `\nconsole.log('Thanks for using this calculator (:');`);
